6a84eac4b866361355d086f302006eb01eb301f8
JonahHummel <jonah-hummel@outlook.com>
2024-02-23 21:46:03 -0600

    initial commit

---
+++ src/main/java/model/ListPet.java
@@ -0,0 +1,13 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "pets")
+public class ListPet {
+	
+}
