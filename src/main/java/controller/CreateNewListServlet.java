246734d9dcb7094d2739c8b13285853269ccff09
Ethan-Tran-115 <156397509+Ethan-Tran-115@users.noreply.github.com>
2024-02-26 10:06:16 -0600

    Add CreateNewList and Navigation servlet, EditPet jsp

--- src/main/java/controller/CreateNewListServlet.java
+++ src/main/java/controller/CreateNewListServlet.java
@@ -1,16 +1,24 @@
 package controller;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.ListDetails;
+import model.ListPet;
+import model.Shopper;
+
 /**
  * Servlet implementation class CreateNewListServlet
  */
-@WebServlet("/CreateNewListServlet")
+@WebServlet("/createNewListServlet")
 public class CreateNewListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,8 +34,42 @@ public class CreateNewListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ListPetHelper lih = new ListPetHelper();
+        String listName = request.getParameter("listName");
+
+        String month = request.getParameter("month");
+        String day = request.getParameter("day");
+        String year = request.getParameter("year");
+
+        String shopperName = request.getParameter("shopperName");
+
+        LocalDate ld;
+        try {
+            ld = LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+        } catch (NumberFormatException ex) {
+            ld = LocalDate.now();
+        }
+
+        String[] selectedListPets = request.getParameterValues("allListPetsToAdd");
+        List<ListPet> selectedPetsInList = new ArrayList<>();
+        // Make sure something was selected – otherwise, we get a null pointer exception
+        if (selectedListPets != null && selectedListPets.length > 0) {
+            for (int i = 0; i < selectedListPets.length; i++) {
+                ListPet listPet = lih.searchForPetById(Integer.parseInt(selectedListPets[i]));
+                selectedPetsInList.add(listPet);
+            }
+        }
+
+        Shopper shopper = new Shopper(shopperName);
+        ListDetails sld = new ListDetails(listName, ld, shopper);
+        sld.setListOfListPets(selectedPetsInList);
+        ListDetailsHelper slh = new ListDetailsHelper();
+        slh.insertNewListDetails(sld);
+
+        System.out.println("Success!");
+        System.out.println(sld.toString());
+
+        getServletContext().getRequestDispatcher("/viewAllListsServlet").forward(request, response);
 	}
 
 	/**
--- src/main/webapp/edit-pet.jsp
+++ src/main/webapp/edit-pet.jsp
@@ -1,12 +1,19 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Edit Pet</title>
 </head>
 <body>
-
+	<form action="editPetServlet" method="post">
+        Species: <input type="text" name="species" value="${petToEdit.species}"><br />
+        Gender: <input type="text" name="gender" value="${petToEdit.gender}"><br />
+        Age: <input type="text" name="age" value="${petToEdit.age}"><br />
+        <input type="hidden" name="id" value="${petToEdit.id}">
+        <input type="submit" value="Save Edited Pet">
+    </form>
 </body>
 </html>
